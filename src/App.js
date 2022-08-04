5e585d874edaac8ec57f8aa62b345462cda0a784
ratspin <6210110013@psu.ac.th>
2022-08-05 03:04:49 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  Header,
+  RecipeImage,
+  AppName,
+  SearchBox,
+  SearchIcon,
+  SearchInput,
+} from './components/header'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Container>
+        <Header>
+          <AppName>
+            <RecipeImage src="/finder/hamburger.svg" />
+            Recipe Finder
+          </AppName>
+          <SearchBox>
+            <SearchIcon src="/finder/search-icon.svg" />
+            <SearchInput
+              placeholder="Search Recipe"
+              // value={searchQuery}
+              // onChange={onTextChange}
+            />
+          </SearchBox>
+        </Header>
+        pai
+      </Container>
+
+
+
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+background-color: black;
+color: white;
+display: flex;
+justify-content: space-between;
+flex-direction: row;
+align-items: center;
+padding: 20px;
+font-size: 25px;
+font-weight: bold;
+box-shadow: 0 3px 6px 0 #555;
+`;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+  `;
+export const RecipeImage = styled.img`
+  width: 36px;
+  height: 36px;
+  margin: 15px;
+`;
+export const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+export const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
