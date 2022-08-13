c2e002f95cd3557aa51048ae2de56176cf3cd7b9
ratspin <6210110013@psu.ac.th>
2022-08-14 04:54:52 +0700

    filter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecipeFinder from './page/RecipeFinder'
 import RecipeFinder2 from './page/RecipeFinder2'
-import CheckboxLabels from './page/CheckboxLabels'
+import CheckboxLabels from './page/filter/CheckboxLabels'
+import Filter from './page/filter/Filter_food'
 
 export default function App() {
   return (
       <div>
         <BrowserRouter>
         <Routes>
-          <Route path='/' element={<RecipeFinder  />} />
+          <Route path='/' element={<Filter  />} />
           <Route path='/RecipeFinder' element={<RecipeFinder />} />
           <Route path='/RecipeFinder2' element={<RecipeFinder2 />} />
           <Route path='/CheckboxLabels' element={<CheckboxLabels />} />
