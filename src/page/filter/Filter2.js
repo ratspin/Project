37b5d7926fc51a408acabd2f1cf61d4198067b78
ratspin <6210110013@psu.ac.th>
2022-10-02 19:48:38 +0700

    test

--- src/page/filter/Filter2.js
+++ src/page/filter/Filter2.js
@@ -15,7 +15,7 @@ export default function Filter2() {
                     <div className="checkbox-container">
                         <div onChange = {() => sethealthLabels("peanut-free")} >    <input type="checkbox" /><label>ถั่วลิสง   </label>    </div><br/>
                         <div onChange = {() => sethealthLabels("soy-free")}>        <input type="checkbox" /><label>ถั่วเหลือง </label>    </div><br/>
-                        <div onChange = {() => sethealthLabels("crustacean-free")}> <input type="checkbox" /><label>ปลา     </label>    </div><br/>
+                        <div onChange = {() => sethealthLabels("fish-free")}> <input type="checkbox" /><label>ปลา     </label>    </div><br/>
                         <div onChange = {() => sethealthLabels("shellfish-free")} > <input type="checkbox" /><label>หอย     </label>    </div><br/>
                         <div onChange = {() => sethealthLabels("crustacean-free")}> <input type="checkbox" /><label>กุ้ง      </label>     </div><br/>
                         <div onChange = {() => sethealthLabels("dairy-freee")}>     <input type="checkbox" /><label>นม      </label>     </div><br/>
